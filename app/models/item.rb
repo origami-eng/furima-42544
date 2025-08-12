0e56de7675708110282b04e0ac8e046d1bc725fc
origami-eng <kt.goto19990427@gmail.com>
2025-08-12 10:52:00 +0900

    Item Listing Development 2
    MVC relation created (not performed testing)

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,12 @@ class Item < ApplicationRecord
   validates :price,                  presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :shipping_time_id,       numericality: { other_than: 1 }
   validates :user,                   presence: true
+  validates :image,                  presence: true
 
   belongs_to :user
   belongs_to :condition
   belongs_to :shipping_fee_charge
   belongs_to :state
   belongs_to :shipping_time
+  has_one_attached :image
 end
