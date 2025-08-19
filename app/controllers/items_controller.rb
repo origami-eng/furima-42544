d0c04afd182f248d495b125ac2de4ed7126ec535
origami-eng <kt.goto19990427@gmail.com>
2025-08-19 21:26:35 +0900

    Item Edit Development 2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item = Item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
